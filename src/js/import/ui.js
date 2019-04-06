f0bbb417aba8497978c9e4c63e8f66ba5bed5d02
Eugene Pro <eugenepro@icloud.com>
2019-04-06 12:56:46 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import objectFitImages from 'object-fit-images';
-import selectize from 'selectize';
-
+import '../lib/selectize';
+import '../lib/maskedinput.js';
 
 
 $('select').selectize({
   sortField: 'text'
 });
+
+$('.phone').mask('+38 (999) 999-9999');
